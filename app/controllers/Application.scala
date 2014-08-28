b95d5a5ede1408063bb8d638c89e6a41d7364871
Jonathan Borzilleri <jonathan@borzilleri.net>
2014-08-28 14:38:58 -0700

    Secures most pages, adds user json to output html
    Most urls routed through the Application controller are now secured, and
    redirect to the index if an unauthenticated/unauthorized user attempts to
    access them.
    
    User information is rendered to the html output as a json object, so we can
    easily see at the JS level if we have a current user.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,44 +1,47 @@
 package controllers
 
-import io.rampant.vulgar.security.AuthTokenManager
-import io.rampant.vulgar.utils.UserAgent
-import models.UserToken
-import models.db.{UserEmails, UserTokens, Users}
-import play.api.libs.json.{JsArray, JsObject, Json}
+import io.rampant.vulgar.security.{AuthRequest, Secured}
+import play.api.libs.json.Json
 import play.api.mvc._
 
-import scala.concurrent.Future
-
 object Application extends Controller {
+	val unauthorizedResult = Redirect(routes.Application.index())
 
-	def main(title: String) = Action {
-		implicit request =>
-			Ok(views.html.index(title))
+	def main(title: String) = {
+		implicit request: AuthRequest[AnyContent] =>
+			Ok(views.html.index(title, request.user.map(Json.toJson(_).toString())))
 	}
 
 	/**
 	 * All Discussions
 	 */
-	def index = main("All Discussions")
+	def index = Secured.Authenticated(main("All Discussions"))
 
 	/**
 	 * Topic View
 	 */
-	def showTopic(slug: String) = main(slug)
+	def showTopic(slug: String) = (Secured.Authenticated andThen Secured.Authorized(result = unauthorizedResult))(
+		main(slug)
+	)
 
 	/**
 	 * Create New Topic
 	 */
-	def createTopic = main("Create Topic")
+	def createTopic = (Secured.Authenticated andThen Secured.Authorized(result = unauthorizedResult))(
+		main("Create Topic")
+	)
 
 	/**
 	 * View User's Profile
 	 */
-	def viewProfile(username: String) = main(username + "'s Profile")
+	def viewProfile(username: String) = (Secured.Authenticated andThen Secured.Authorized(result = unauthorizedResult))(
+		main(s"$username's Profile")
+	)
 
 	/**
 	 * Edit Your Profile
 	 */
-	def editProfile = main("Edit Your Profile")
-
+	def editProfile = (Secured.Authenticated andThen Secured.Authorized(result = unauthorizedResult))(
+		main("Edit Your Profile")
+	)
 }
